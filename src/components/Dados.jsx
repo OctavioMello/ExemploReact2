2d16ea7bd30c33a5d5174a2991254abd6db22986
Octávio <octaviomellocovredesouza@gmail.com>
2026-08-21 09:06:11 -0300

    feat: Componente Dados usando destruct(desestruturação)

---
+++ src/components/Dados.jsx
@@ -0,0 +1,11 @@
+
+const Dados = ({usuario, email}) => {
+  return (
+    <>
+      <p>Nome do Usuário: {usuario}</p>
+      <p>E-Mail do Usuário: {email}</p>
+    </>
+  )
+}
+
+export default Dados
